9a2020425a3324160c9ee0d45ca9776c2b490c4d
DarkZ <93233687+MirrorDevelopers@users.noreply.github.com>
2021-12-18 23:40:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,13 +72,13 @@ async def mizuki(client, message):
 @bot.on_message(filters.command("start") & ~filters.edited)
 async def start(client, message):
    if message.chat.type == 'private':
-       await message.reply("**Hey There, I'm Mizuki. An advanced chatbot with AI. \n\nAdd me to your group and chat with me!**",   
+       await message.reply("**Hey There, I'm SlapTap. An advanced chatbot with AI. \n\nAdd me to your group and chat with me!**",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
-                                            "Dev", url="https://t.me/MizukiUpdatesOfficial"),
+                                            "Updates Channel🥳", url="https://t.me/SlapTap"),
                                         InlineKeyboardButton(
-                                            "Repo", url="https://github.com/ImJanindu/Mizuki-Chat-Bot")
+                                            "Source Code📝", url="github.com")
                                     ]]
                             ),               
            )
